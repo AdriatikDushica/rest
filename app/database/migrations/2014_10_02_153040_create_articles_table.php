925d553e90f2731cc580392af0406d0248fd53e6
Adriatik Dushica <adriatik.dushica@gmail.com>
2014-10-26 15:05:04 +0100

    added database migrations

---
+++ app/database/migrations/2014_10_02_153040_create_articles_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateArticlesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('articles', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('title');
+			$table->text('body');
+			$table->timestamps();
+		});
+
+        for ($i = 0; $i < 10; $i++) {
+            Article::create([
+                'title' => "Title ({$i})",
+                'body' => "content number ({$i})"
+            ]);
+        }
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('articles');
+	}
+
+}
